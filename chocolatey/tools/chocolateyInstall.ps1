2265f46abe3d182fbaaf4bcead86a276c35e4f3e
EDAMAME Dev Local <dev@edamame.tech>
2025-12-02 12:08:48 +0100

    Update release workflow: adjust timeout for musl aarch64 builds, simplify Rust installation conditions, and increment version number to 0.9.82 in Chocolatey package files.

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 $packageName = 'edamame-cli'
+$packageVersion = '0.9.82'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/edamametechnologies/edamame_cli/releases/download/v0.9.81/edamame_cli-0.9.81-x86_64-pc-windows-msvc.exe'
-$checksum64 = '0000000000000000000000000000000000000000000000000000000000000000'
+$url64 = "https://github.com/edamametechnologies/edamame_cli/releases/download/v$packageVersion/edamame_cli-$packageVersion-x86_64-pc-windows-msvc.exe"
+$checksum64 = '969ca28f90a53ddb909d6a4b1b527afa797c6bb3ea782ad745d55bf0324f3e97'
 
 Install-ChocolateyPackage -PackageName $packageName `
                           -FileType 'exe' `
