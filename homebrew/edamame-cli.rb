57ee825a94bee63f99ca30f9dbf740a26c815f3e
franklyonnet <flyonnet@edamame.tech>
2026-02-20 15:47:47 +0800

    Pre-publish

--- homebrew/edamame-cli.rb
+++ homebrew/edamame-cli.rb
@@ -1,9 +1,9 @@
 class EdamameCli < Formula
   desc "EDAMAME CLI interface to EDAMAME Posture service"
   homepage "https://edamame.tech"
-  url "https://github.com/edamametechnologies/edamame_cli/releases/download/v1.0.6/edamame_cli-1.0.6-universal-apple-darwin"
+  url "https://github.com/edamametechnologies/edamame_cli/releases/download/v1.0.7/edamame_cli-1.0.7-universal-apple-darwin"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-  version "1.0.6"
+  version "1.0.7"
   license "Apache-2.0"
 
   def install
