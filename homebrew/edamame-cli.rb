c568ee3f2b542a2694adcbf8529031f28ec5c2b2
EDAMAME Dev Local <dev@edamame.tech>
2026-04-22 21:32:05 +0200

    Update edamame_cli to version 1.2.2 and adjust dependencies in Cargo.lock
    - Bump edamame_cli version from 1.2.1 to 1.2.2 in Cargo.toml and chocolatey files.
    - Update dependencies in Cargo.lock, including the rand crate to version 0.8.6 and various other packages to their latest versions.
    - Add new symlink package to Cargo.lock.
    - Update Homebrew formula to reflect the new version and URL for edamame_cli.

--- homebrew/edamame-cli.rb
+++ homebrew/edamame-cli.rb
@@ -1,9 +1,9 @@
 class EdamameCli < Formula
   desc "EDAMAME CLI interface to EDAMAME Posture service"
   homepage "https://edamame.tech"
-  url "https://github.com/edamametechnologies/edamame_cli/releases/download/v1.2.1/edamame_cli-1.2.1-universal-apple-darwin"
+  url "https://github.com/edamametechnologies/edamame_cli/releases/download/v1.2.2/edamame_cli-1.2.2-universal-apple-darwin"
   sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-  version "1.2.1"
+  version "1.2.2"
   license "Apache-2.0"
 
   def install
